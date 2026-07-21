742dc8e1f0911fda18bf95ee5bbc82a663471ddc
portalsimonsports <portalsimonsports@gmail.com>
2026-07-21 13:59:52 -0300

    Adicionar configuração independente do torneio

---
+++ config.js
@@ -0,0 +1,15 @@
+window.VOLEI_CONFIG = Object.freeze({
+  APP_NAME: 'Sorteio de Duplas de Vôlei',
+  API_BASE: '',
+  SHEET_ID: '1lg0HKljL93wD5riajKbCYcShzKYW0qAVYkPTwjerVAo',
+  SHEET_URL: 'https://docs.google.com/spreadsheets/d/1lg0HKljL93wD5riajKbCYcShzKYW0qAVYkPTwjerVAo/edit',
+  DEMO_MODE: true,
+  POLL_INTERVAL_MS: 5000,
+  BEST_OF_SETS: 3,
+  SETS_TO_WIN: 2,
+  NORMAL_SET_POINTS: 25,
+  TIEBREAK_SET_POINTS: 15,
+  MINIMUM_LEAD: 2,
+  MATCH_INTERVAL_MINUTES: 10,
+  VERSION: 'V005_GITHUB_INDEPENDENTE_VOLEI_QUADRA_2026-07-21'
+});
